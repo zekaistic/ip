8ebad1f545dfddd846e54b514487a1b4402e207b
Yap Ze Kai <yapzekai11@gmail.com>
2025-08-20 00:19:11 +0800

    Add mark as done feature

--- src/main/java/Chungus.java
+++ src/main/java/Chungus.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Chungus {
     public static void main(String[] args) {
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int listIndex = 0;
 
         Scanner sc = new Scanner(System.in);
@@ -16,12 +16,31 @@ public class Chungus {
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 System.out.println("____________________________________________________________\n");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < listIndex; i++) {
-                    System.out.println(String.format("%d. %s", i + 1, list[i]));
+                    System.out.println(String.format("%d. %s", i + 1, list[i].toString()));
                 }
                 System.out.println("____________________________________________________________\n");
+            } else if (input.startsWith("mark ")) {
+                int idx = Integer.parseInt(input.substring(5).trim()) - 1;
+                if (idx >= 0 && idx < listIndex) {
+                    list[idx].markAsDone();
+                    System.out.println("____________________________________________________________\n");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(String.format("  %s", list[idx].toString()));
+                    System.out.println("____________________________________________________________\n");
+                }
+            } else if (input.startsWith("unmark ")) {
+                int idx = Integer.parseInt(input.substring(7).trim()) - 1;
+                if (idx >= 0 && idx < listIndex) {
+                    list[idx].markAsNotDone();
+                    System.out.println("____________________________________________________________\n");
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(String.format("  %s", list[idx].toString()));
+                    System.out.println("____________________________________________________________\n");
+                }
             } else {
-                list[listIndex] = input;
+                list[listIndex] = new Task(input);
                 listIndex++;
                 System.out.println("____________________________________________________________\n");
                 System.out.println(String.format("added: %s", input));
