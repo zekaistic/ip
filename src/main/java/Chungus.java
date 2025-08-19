fae15a96c97a5d1bbbb189bc23f8e5676a5137a8
Yap Ze Kai <yapzekai11@gmail.com>
2025-08-19 23:53:36 +0800

    Add list

--- src/main/java/Chungus.java
+++ src/main/java/Chungus.java
@@ -2,17 +2,31 @@ import java.util.Scanner;
 
 public class Chungus {
     public static void main(String[] args) {
+        String[] list = new String[100];
+        int listIndex = 0;
+
         Scanner sc = new Scanner(System.in);
         String message = "____________________________________________________________\n"
                 + "Hello! I'm Chungus!\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n";
+                + "____________________________________________________________\n";        
         System.out.println(message);
+
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println("____________________________________________________________\n");
-            System.out.println(input);
-            System.out.println("____________________________________________________________\n");
+            if (input.equals("list")) {
+                System.out.println("____________________________________________________________\n");
+                for (int i = 0; i < listIndex; i++) {
+                    System.out.println(String.format("%d. %s", i + 1, list[i]));
+                }
+                System.out.println("____________________________________________________________\n");
+            } else {
+                list[listIndex] = input;
+                listIndex++;
+                System.out.println("____________________________________________________________\n");
+                System.out.println(String.format("added: %s", input));
+                System.out.println("____________________________________________________________\n");
+            }
             input = sc.nextLine();
         }
         System.out.println("____________________________________________________________\n");
