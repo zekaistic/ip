71231a6310e313023f0b6e1da3625f73fab513e5
Yap Ze Kai <yapzekai11@gmail.com>
2025-08-19 23:07:01 +0800

    Add Chungus.java, removed Duke.java

---
+++ src/main/java/Chungus.java
@@ -0,0 +1,11 @@
+public class Chungus {
+    public static void main(String[] args) {
+        String message = "____________________________________________________________\n"
+                + "Hello! I'm Chungus!\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+        System.out.println(message);
+    }
+}
