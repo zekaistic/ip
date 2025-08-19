19367502e5b4578e28f6a57624049a43d8b1ec44
Yap Ze Kai <yapzekai11@gmail.com>
2025-08-19 23:27:41 +0800

    Add Echo

--- src/main/java/Chungus.java
+++ src/main/java/Chungus.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Chungus {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String message = "____________________________________________________________\n"
                 + "Hello! I'm Chungus!\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
+                + "____________________________________________________________\n";
         System.out.println(message);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println("____________________________________________________________\n");
+            System.out.println(input);
+            System.out.println("____________________________________________________________\n");
+            input = sc.nextLine();
+        }
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________\n");
+        sc.close();
     }
 }
